d1c936817fcdf70de98a06c9dec0bb5e1fc0476b
Elsa <elsahvy@gmail.com>
2022-12-12 11:01:15 -0800

    vercel test2

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>New laughing coyote!!</h1>
+      <p>another test</p>
     </div>
   );
 }
