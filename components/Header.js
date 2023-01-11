8a3cc20f46dfeb8be9096aa098456ef9987cda49
Elsa <elsahvy@gmail.com>
2023-01-11 14:05:13 -0800

    add framer motion

--- components/Header.js
+++ components/Header.js
@@ -16,38 +16,47 @@ import {
   AiOutlineMail,
 } from "react-icons/ai";
 import JtPic from "../public/images/joshuatreeabout.jpg";
+import { motion } from "framer-motion";
 
 const HeaderSection = () => {
   return (
     <>
-      <Header>
-        <HeaderItems>
-          <Logo>Photography Portfolio Template</Logo>
-          <p>Hi there, Im Elsa. Im a web developer based in Joshua Tree, CA.</p>
-          <SmIcons>
-            <Link href="">
-              <AiOutlineMail />
-            </Link>
-            <a href="https://linkedin.com/">
-              <AiFillLinkedin />
-            </a>
-            <a href="https://instagram.com/">
-              <AiOutlineInstagram />
-            </a>
-          </SmIcons>
-        </HeaderItems>
-        <MobileIntro>
-          <Intro />
-        </MobileIntro>
-        <AboutImgWrapper>
-          <Image
-            className="portfolio-img"
-            src={JtPic}
-            alt="elsa hovey"
-            layout="responsive"
-          />
-        </AboutImgWrapper>
-      </Header>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+      >
+        <Header>
+          <HeaderItems>
+            <Logo>Photography Portfolio Template</Logo>
+            <p>
+              Hi there, Im Elsa. Im a web developer based in Joshua Tree, CA.
+            </p>
+            <SmIcons>
+              <Link href="">
+                <AiOutlineMail />
+              </Link>
+              <a href="https://linkedin.com/">
+                <AiFillLinkedin />
+              </a>
+              <a href="https://instagram.com/">
+                <AiOutlineInstagram />
+              </a>
+            </SmIcons>
+          </HeaderItems>
+          <MobileIntro>
+            <Intro />
+          </MobileIntro>
+          <AboutImgWrapper>
+            <Image
+              className="portfolio-img"
+              src={JtPic}
+              alt="elsa hovey"
+              layout="responsive"
+            />
+          </AboutImgWrapper>
+        </Header>
+      </motion.div>
     </>
   );
 };
